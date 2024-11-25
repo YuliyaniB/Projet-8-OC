e70e7215c4e9a6bfe69beda7d51489736b619222
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-11-25 15:28:07 +0100

    first commit

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,11 +1,11 @@
 export const mainMenu = [
-     {link:"#", content:"Home", icon:"lucide:home"},
-     {link:"#about", content:"About", icon:"octicon:person-24"},
+     {link:"#", content:"Accueil", icon:"lucide:home"},
+     {link:"#about", content:"À propos", icon:"octicon:person-24"},
      {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
-     {link:"#blog", content:"Blog", icon:"mdi:blog"}
+     /*{link:"#blog", content:"Blog", icon:"mdi:blog"}*/
 ]
 export const menuBlog = [
-     {link:"/", content:"Home", icon:"lucide:home"},
-     {link:"/#about", content:"About", icon:"octicon:person-24"},
+     {link:"/", content:"Accueil", icon:"lucide:home"},
+     {link:"/#about", content:"À propos", icon:"octicon:person-24"},
      {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
 ]
