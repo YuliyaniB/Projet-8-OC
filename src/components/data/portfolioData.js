60b9d5f54dfec885f37ee21878fd301f61cf054f
YuliyaniB <166741190+YuliyaniB@users.noreply.github.com>
2024-11-27 11:46:00 +0100

    dernierers modifications

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -12,12 +12,12 @@
  */
 
 /**
- * @SofiDev Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
+ * 
  * @type {PortafolioData[]}
  */
 export const portafolioData = [
 	{
-		imgSrc: '/img/booki.png',
+		imgSrc: '/img/booki.webp',
 		title: 'Booki',
 		skills: ['HTML', 'CSS'],
 		descripcion:
@@ -27,56 +27,54 @@ export const portafolioData = [
 		anim: 'fade-right',
 	},
 	{
-		imgSrc: '/img/sophiebluel.png',
+		imgSrc: '/img/sophiebluel.webp',
 		title: 'Sophie Bluel',
 		skills: ['JavaScript'],
 		descripcion:
-			'Créez une page web dynamique avec JavaScript',
+			"Développement d'un site web dynamique pour Sophie Bluel, architecte d'intérieur. Ce projet OpenClassrooms m'a permis de renforcer mes compétences en JavaScript, en particulier sur l'intégration d'une API pour afficher des projets en temps réel. Résultat : un site moderne, fonctionnel et interactif, offrant une expérience utilisateur fluide.",
 		demoURL: '',
 		repoURL: 'https://github.com/YuliyaniB/Projet-3-OC',
 		anim: 'fade-up',
 		averageBrightness: 0.1,
 	},
 	{
-		imgSrc: '/img/Dashboard.png',
+		imgSrc: '/img/ninacarducci.webp',
 		title: 'Nina Carducci',
-		skills: ['React', 'Lighthouse'],
-		descripcion:
-			'Débuggez et optimisez un site de photographe',
+		skills: ['', 'Lighthouse'],
+		descripcion: "Analyse, correction et optimisation du site web de Nina Carducci, photographe professionnelle. Ce projet OpenClassrooms m'a permis de renforcer mes compétences en résolution de bugs, optimisation des performances et amélioration de l'accessibilité web. Résultat : un site fonctionnel, fluide et conforme aux standards modernes.",
 		demoURL: 'https://yuliyanib.github.io/Projet-4-OC/',
 		repoURL: 'https://github.com/YuliyaniB/Projet-4-OC',
 		anim: 'fade-left',
 	},
 	{
-		imgSrc: '/img/kasa.png',
+		imgSrc: '/img/kasa.webp',
 		title: 'Kasa',
 		skills: ['React', 'Sass'],
 		descripcion:
-			'Créez une application web de location immobilière avec React',
+			"Développement d'une application web pour Kasa, une plateforme de location. Ce projet OpenClassrooms m'a permis de consolider mes compétences en React, notamment avec la gestion des états, des composants dynamiques et la navigation. Le résultat : une interface intuitive, moderne et entièrement responsive pour une expérience utilisateur optimale.",
 		demoURL: '',
 		repoURL: 'https://github.com/YuliyaniB/Projet-5-OC',
 		anim: 'fade-left',
 	},
 	{
-		imgSrc: '/img/vieuxgrimoire.png',
+		imgSrc: '/img/vieuxgrimoire.webp',
 		title: 'Mon vieux grimoire',
 		skills: ['NodeJS', 'MongoDB'],
-		descripcion:
-			"Développez le back-end d'un site de notation de livres",
+		descripcion: "Développement d'une application web sécurisée pour gérer une bibliothèque personnelle. Ce projet OpenClassrooms m'a permis d'acquérir des compétences en Node.js, Express et MongoDB, notamment sur la création d'API RESTful et la gestion de l'authentification. Résultat : une application robuste et intuitive pour cataloguer et partager des livres.",
 		demoURL: '',
 		repoURL: 'https://github.com/YuliyaniB/Projet-6-OC',
 		anim: 'fade-left',
 	},
 	{
-		imgSrc: '/img/menumaker.png',
+		imgSrc: '/img/menumaker.webp',
 		title: 'Menu Maker',
-		skills: ['React', 'StyledComponents'],
-		descripcion:
-			'Planifiez le développement du site de votre client',
-		demoURL: '',
+		skills: ['Notion', ''],
+		descripcion: "Organisation et gestion du projet MenuMaker en utilisant des outils de productivité tels que Notion et Feedly. Ce projet OpenClassrooms m'a permis de structurer le processus de développement, de planifier les tâches et d'assurer une veille continue pour améliorer l'efficacité et la qualité du projet. Résultat : une gestion fluide du projet, garantissant le respect des délais et des objectifs.",
+		demoURL:
+			'https://bejewled-aardvark-968.notion.site/128ecc2129e380e08956c73bb148ff49?v=128ecc2129e3817fa48b000cd87918c4',
 		repoURL: '',
 		anim: 'fade-right',
-	}
+	},
 ];
 
 const skillIcons = {
@@ -90,7 +88,8 @@ const skillIcons = {
 	NodeJS: 'skill-icons:nodejs',
 	Express: 'skill-icons:express',
 	MongoDB: 'skill-icons:mongodb',
-	Lighthouse: 'skill-icons:mysql'
+	Lighthouse: 'skill-icons:mysql',
+	Notion: 'skill-icons:Notion-Light'
 };
 
 /**
